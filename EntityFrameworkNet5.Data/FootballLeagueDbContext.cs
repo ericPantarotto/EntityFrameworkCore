a96b8847ebaa6dab3dfa236d02b04e95c4aae3ae
Eric Carlier <eric.carlier@gmail.com>
2022-03-06 23:06:16 +0100

    Data provider and NugetSetup

--- EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
@@ -1,9 +1,15 @@
+using EntityFrameworkCore.Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameworkCore.Data
 {
     public class FootBallLeagueDbContext : DbContext
     {
-        // public DbSet<Team> Teams {get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\MSSQLLocalDb; Initial Catalog= FootballLeague_EFCore");
+        }
+        public DbSet<Team> Teams {get; set; }
+        public DbSet<League> Leagues { get; set; }
     }  
 }
