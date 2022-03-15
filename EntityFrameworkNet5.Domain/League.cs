614cda0a51c036404159d6ef2f424ce39120b492
Eric Carlier <eric.carlier@gmail.com>
2022-03-15 14:49:19 +0100

    Adding Teams to League

--- EntityFrameworkNet5.Domain/League.cs
+++ EntityFrameworkNet5.Domain/League.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+
 namespace EntityFrameworkCore.Domain
 {
     public class League
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<Team> Teams { get; set; }
     }
     
 }
