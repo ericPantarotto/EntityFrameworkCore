032d9877ecee499e1023948ef404bd4a25de8b62
Eric Carlier <eric.carlier@gmail.com>
2022-03-22 09:26:11 +0100

    Adding View to Domain

---
+++ EntityFrameworkNet5.Domain/TeamCoachesLeaguesView.cs
@@ -0,0 +1,9 @@
+namespace EntityFrameworkCore.Domain
+{
+    public class TeamCoachesLeagues
+    {
+        public string Name { get; set; }
+        public string CoachName { get; set; }
+        public string LeagueName { get; set; }
+    }
+}
