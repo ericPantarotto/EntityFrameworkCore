0d87f91a03d9481fb819f86a264b724e8906cb2f
Eric Carlier <eric.carlier@gmail.com>
2022-03-25 21:06:07 +0100

    Manipulate Entries - Data

--- EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using EntityFrameworkCore.Domain;
 using EntityFrameworkNet5.Data;
 using Microsoft.EntityFrameworkCore;
@@ -37,6 +40,21 @@ namespace EntityFrameworkCore.Data
             modelBuilder.ApplyConfiguration(new TeamSeedConfiguration());
             modelBuilder.ApplyConfiguration(new CoachSeedConfiguration());
         } 
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken =default)
+        {
+            var entries = ChangeTracker.Entries().Where(q => q.State == EntityState.Added || q.State == EntityState.Modified).ToList();
+            entries.ForEach(entry => 
+            {
+                var auditableObject = (BaseDomainObject)entry.Entity;
+                auditableObject.ModifiedDate = DateTime.Now;
+                if (entry.State == EntityState.Added)
+                {
+                    auditableObject.CreatedDate = DateTime.Now;
+                }
+            });
+            return await base.SaveChangesAsync(cancellationToken);
+        }
         public DbSet<Team> Teams {get; set; }
         public DbSet<League> Leagues { get; set; }
         public DbSet<Match> Matches { get; set; }
