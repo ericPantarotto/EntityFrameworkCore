9a5f44913c8efbd3c76115364e294b50c85f9914
Eric Carlier <eric.carlier@gmail.com>
2022-03-25 12:40:32 +0100

    Renaming Audit Fields

--- EntityFrameworkNet5.Domain/Common/BaseDomainObject.cs
+++ EntityFrameworkNet5.Domain/Common/BaseDomainObject.cs
@@ -5,9 +5,9 @@ namespace EntityFrameworkCore.Domain
     public class BaseDomainObject
     {
         public int Id { get; set; }
-        public DateTime DateCrated { get; set; }
-        public DateTime LastModified { get; set; }
-        public string Created { get; set; }
-        public string Modified { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
     }
 }
