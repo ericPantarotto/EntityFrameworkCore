f0402fd7026e7f96700fc709a8fdcf2f1ef4d126
Eric Carlier <eric.carlier@gmail.com>
2022-03-25 11:55:52 +0100

    Setting Domain to Rollback

--- EntityFrameworkNet5.Domain/Common/BaseDomainObject.cs
+++ EntityFrameworkNet5.Domain/Common/BaseDomainObject.cs
@@ -3,5 +3,9 @@ namespace EntityFrameworkCore.Domain
     public class BaseDomainObject
     {
         public int Id { get; set; }
+        public DateTime DateCrated { get; set; }
+        public DateTime LastModified { get; set; }
+        public string Created { get; set; }
+        public string Modified { get; set; }
     }
 }
