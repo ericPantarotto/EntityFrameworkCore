469af033fb4e4ee0fdc54329a1c528d15937fb7d
Eric Carlier <eric.carlier@gmail.com>
2022-03-29 09:55:41 +0200

    Domain Audit, addin action field

--- EntityFrameworkNet5.Domain/Audit.cs
+++ EntityFrameworkNet5.Domain/Audit.cs
@@ -5,10 +5,12 @@ namespace EntityFrameworkCore.Domain
     public class Audit
     {
         public int Id { get; set; }
+        public string Action { get; set; }          
         public string TableName { get; set; }          
         public DateTime DateTime { get; set; }
         public string KeyValues { get; set; }
         public string OldValues { get; set; }
         public string NewValues { get; set; }
+
     }
 }
