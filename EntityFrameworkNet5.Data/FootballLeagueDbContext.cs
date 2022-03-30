3ceddc4d0c79b4b7c21c6fb3b5fb840781f0a0b6
Eric Carlier <eric.carlier@gmail.com>
2022-03-30 19:24:29 +0200

    Data Validation

--- EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
@@ -36,6 +36,16 @@ namespace EntityFrameworkCore.Data
             
             modelBuilder.Entity<TeamCoachesLeaguesView>().HasNoKey().ToView("TeamsCoachesLeagues");
 
+            modelBuilder.Entity<Team>().Property(p => p.Name).HasMaxLength(50);
+            modelBuilder.Entity<Team>().HasIndex(h => h.Name);
+            
+            modelBuilder.Entity<League>().Property(p => p.Name).HasMaxLength(50);
+            modelBuilder.Entity<League>().HasIndex(h => h.Name).IsUnique();
+
+            modelBuilder.Entity<Coach>().Property(p => p.Name).HasMaxLength(50);
+            modelBuilder.Entity<Coach>().HasIndex(h => new {h.Name, h.TeamId}).IsUnique();
+
+
             modelBuilder.ApplyConfiguration(new LeagueSeedConfiguration());
             modelBuilder.ApplyConfiguration(new TeamSeedConfiguration());
             modelBuilder.ApplyConfiguration(new CoachSeedConfiguration());
--- EntityFrameworkNet5.Data/Migrations/20220330170750_AddedValidations.Designer.cs
+++ EntityFrameworkNet5.Data/Migrations/20220330170750_AddedValidations.Designer.cs
@@ -4,14 +4,16 @@ using EntityFrameworkCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EntityFrameworkNet5.Data.Migrations
 {
     [DbContext(typeof(FootBallLeagueDbContext))]
-    partial class FootBallLeagueDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220330170750_AddedValidations")]
+    partial class AddedValidations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
